8403723ccd7320740ccd72d855b85bc1be0a72f0
Evan <evan.horne@oooska.com>
2015-06-13 18:34:00 -0400

    Support for 'auto' and 'stop' drive commands

--- drive.py
+++ drive.py
@@ -8,13 +8,13 @@ class drive:
 		return "This should return a 405 - Not Supported Error"
 	
 	#POST excepts a single command in plain teext.
-	#Current supported commands: forward', 'reverse', 'left', or 'right'
+	#Current supported commands: forward', 'reverse', 'left', 'right', 'auto', 'stop'
 	def POST(self):
-		print ("web.data: %s" % web.data())	#'forward', 'reverse', 'left', or 'right'
 		direction = web.data()
-		if direction not in ['forward', 'reverse', 'left', 'right']:
+		if direction not in ['forward', 'reverse', 'left', 'right', 'auto', 'stop']:
 			raise errors.BadRequestError
 
 		#Set robot direction here...
+
 
 		return "Updated direction"
